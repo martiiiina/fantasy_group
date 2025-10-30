2356b5189cb6fff2f1afd4f1533867550710976d
martiiiina <martina.baroffio@epfl.ch>
2025-10-30 18:22:07 +0100

    comments

--- implementations.py
+++ implementations.py
@@ -29,6 +29,19 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
 
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
+    """The Gradient Descent (GD) algorithm.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,D)
+        initial_w: numpy array of shape=(D, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        w: last weight vector, for the last iteration
+        loss: mse, for last iteration
+    """
     w = initial_w
     for n_iter in range(max_iters):
         N = y.shape[0]
@@ -43,6 +56,19 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     return w, loss
 
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
+    """The Stochastic Gradient Descent algorithm (SGD).
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,D)
+        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of SGD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        w: last weight vector, for the last iteration
+        loss: mse, for last iteration
+    """
     w = initial_w
     N = y.shape[0]
     for n_iter in range(max_iters):
@@ -54,6 +80,17 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     return w, loss
 
 def least_squares(y, tx):
+    """Calculate the least squares solution.
+       returns optimal weights and mse.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+    """
     A = tx.T @ tx
     b = tx.T @ y
     w = np.linalg.solve(A, b)
